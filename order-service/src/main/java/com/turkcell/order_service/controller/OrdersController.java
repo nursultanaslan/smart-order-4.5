50c096e0410974c101fcf4445dd4bde53634b7cb
nursultanaslan <nursultanaslan1@gmail.com>
2025-10-30 13:29:54 +0300

    add kafka config

--- order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
+++ order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
@@ -1,5 +1,8 @@
 package com.turkcell.order_service.controller;
 
+import com.turkcell.order_service.dto.CreateOrderDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/orders")
 public class OrdersController {
 
+    @PostMapping
+    public String createOrder(@RequestBody CreateOrderDto orderDto) {
+
+    }
 }
