ade44543641552b2c4e1322d11edca655f7b8308
nursultanaslan <nursultanaslan1@gmail.com>
2025-12-17 21:53:11 +0300

    modify client-secret for dev environment

--- gateway-server/src/main/java/com/turkcell/gateway_server/config/SecurityConfig.java
+++ gateway-server/src/main/java/com/turkcell/gateway_server/config/SecurityConfig.java
@@ -3,26 +3,25 @@ package com.turkcell.gateway_server.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 //Her request gatewayden geçerken jwt dogrulamasına tabi tutulur.
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
         @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
                 return http
-                                .csrf(AbstractHttpConfigurer::disable) // csrf devredısı
+                                .csrf(ServerHttpSecurity.CsrfSpec::disable) // csrf devredısı
                                 // hangi isteklere izin verilsin? hangileri dogrulama gerektirsin?
                                 //actuator dışındaki tüm istekler doğrulama gerektirsin.
-                                .authorizeHttpRequests(
+                                .authorizeExchange(
                                                 req -> req
-                                                                .requestMatchers("/actuator/**").permitAll()
-                                                                .anyRequest().authenticated())
+                                                                .pathMatchers("/actuator/**").permitAll()
+                                                                .anyExchange().authenticated())
                                 //(access token validate)
                                 // jwt filter yerine oauth2 kullanıyoruz (jwt validation)
                                 .oauth2ResourceServer(oauth2 ->
