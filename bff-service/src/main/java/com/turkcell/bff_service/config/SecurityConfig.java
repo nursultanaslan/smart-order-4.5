3d65df3e72cb50cc1644799aebf209a7bf7a71c3
nursultanaslan <nursultanaslan1@gmail.com>
2025-12-21 23:26:34 +0300

    added csrf protection

--- bff-service/src/main/java/com/turkcell/bff_service/config/SecurityConfig.java
+++ bff-service/src/main/java/com/turkcell/bff_service/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.server.csrf.ServerCsrfTokenRequestAttributeHandler;
 
 //OAuthClient + Session
 @Configuration
@@ -15,7 +17,12 @@ public class SecurityConfig {
     @Bean
     SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
         return http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .csrf(csrf ->
+                        csrf
+                                //tokeni bir cookie icerisinde sakla.
+                                .csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
+                                // webflux icin tokenin her istekte handle edilmesini sağla.c
+                                .csrfTokenRequestHandler(new ServerCsrfTokenRequestAttributeHandler()))
                 .authorizeExchange(ex -> ex.anyExchange().authenticated())  //requestMatchers
                 .oauth2Login(Customizer.withDefaults())
                 .oauth2Client(Customizer.withDefaults())
