598a398531d7b686f83cc5551d7a6e39879022cc
nursultanaslan <nursultanaslan1@gmail.com>
2026-01-29 13:48:05 +0300

    impl. security configuration for cart service

--- cart-service/src/main/java/com/turkcell/cart_service/config/SecurityConfig.java
+++ cart-service/src/main/java/com/turkcell/cart_service/config/SecurityConfig.java
@@ -2,14 +2,31 @@ package com.turkcell.cart_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-//    @Bean
-//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//
-//    }
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        req -> req
+                                .requestMatchers(
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs/**",
+                                        "/swagger-ui.html"
+                                ).permitAll()
+                                .requestMatchers("/api/v1/carts/**").permitAll()
+                                .requestMatchers("/api/v1/carts/checkout/**").authenticated()
+                )
+                .oauth2ResourceServer(
+                        oauth2 -> oauth2
+                                .jwt(Customizer.withDefaults()))
+                .build();
+    }
 }
