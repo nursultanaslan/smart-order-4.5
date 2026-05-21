020f8153785bc46b19204c3c7da2bac856c7a7cc
nursultanaslan <nursultanaslan1@gmail.com>
2026-05-21 22:33:22 +0300

    add return service for return operations

---
+++ return-service/src/test/java/com/turkcell/returnservice/ReturnServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.returnservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReturnServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
