0e670d2935cdff680c3b0c3f5c6dec76b94961aa
nursultanaslan <nursultanaslan1@gmail.com>
2026-07-02 14:08:05 +0300

    refactor cancel method to improve validation and exception handling

--- order-service/src/main/java/com/turkcell/order_service/domain/exception/ReturnPeriodExpiredException.java
+++
@@ -1,7 +0,0 @@
-package com.turkcell.order_service.domain.exception;
-
-public class ReturnPeriodExpiredException extends RuntimeException {
-    public ReturnPeriodExpiredException(String message) {
-        super(message);
-    }
-}
